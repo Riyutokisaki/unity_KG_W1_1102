0d3960d942e019939421bab3fddade29bd739d8a
Riyutokisaki <90891791+Riyutokisaki@users.noreply.github.com>
2022-03-26 16:27:14 +0800

    嘗試增加一些東西

--- Parkour/Assets/Course Library/Scripts/PlayerController.cs
+++ Parkour/Assets/Course Library/Scripts/PlayerController.cs
@@ -8,9 +8,9 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier; //系統物理重力
     
     public bool isOnground = true; //是否站在地板上
-
     public bool gameOver = false;
     private Animator playAn;
+    
     #endregion
     void Start()
     {
@@ -22,22 +22,33 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isOnground){
+        Jump();
+  
+    }
+
+    //若與地板有接觸(Enter)，則設為 true
+    private void OnCollisionEnter(Collision other) 
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isOnground = true; //表示在地板上(避免二段跳)
+        } else if (other.gameObject.CompareTag("OOO"))
+        {
+            gameOver = true; //因為碰撞到障礙物，表示遊戲結束
+            print("遊戲結束！");  //在 #Console 視窗顯示 "遊戲結束"
+        } 
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnground)
+        {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnground = false;
             playAn.SetTrigger("Jump_trig");
         }
+        if (!isOnground) playAn.SetBool("Lean_b", true);
+        else playAn.SetBool("Lean_b", false);
     }
 
-    //若與地板有接觸(Enter)，則設為 true
-    private void OnCollisionEnter(Collision other) {
-        if (other.gameObject.CompareTag("Ground")){
-            isOnground = true; //表示在地板上(避免二段跳)
-        } else if (other.gameObject.CompareTag("OOO")){
-            gameOver = true; //因為碰撞到障礙物，表示遊戲結束
-            print("遊戲結束！");  //在 #Console 視窗顯示 "遊戲結束"
-        }
-        
-         
-    }
 }
