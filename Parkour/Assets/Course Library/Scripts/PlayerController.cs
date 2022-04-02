38990746a904494500c9c4ce160a07933b69dabf
Riyutokisaki <90891791+Riyutokisaki@users.noreply.github.com>
2022-04-03 03:35:56 +0800

    素材新增

--- Parkour/Assets/Course Library/Scripts/PlayerController.cs
+++ Parkour/Assets/Course Library/Scripts/PlayerController.cs
@@ -10,13 +10,20 @@ public class PlayerController : MonoBehaviour
     public bool isOnground = true; //是否站在地板上
     public bool gameOver = false;
     private Animator playAn;
-    
+
+    public ParticleSystem ps_ex;
+    public ParticleSystem ps_dirty;
+
+    public AudioClip ac_jump;
+    public AudioClip ac_ex;
+    private AudioSource as_player;
     #endregion
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier; // 等同 Physics.gravity * Physics.gravity = gravityModifier
         playAn = GetComponent<Animator>();
+        as_player = GetComponent<AudioSource>();
     }
 
     
@@ -32,10 +39,16 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Ground"))
         {
             isOnground = true; //表示在地板上(避免二段跳)
+            ps_dirty.Play();
         } else if (other.gameObject.CompareTag("OOO"))
         {
             gameOver = true; //因為碰撞到障礙物，表示遊戲結束
             print("遊戲結束！");  //在 #Console 視窗顯示 "遊戲結束"
+            playAn.SetBool("Death_b", true);
+            playAn.SetInteger("DethType_int", 1);
+            ps_ex.Play();
+            ps_dirty.Stop();
+            as_player.PlayOneShot(ac_ex, 1.0f);
         } 
     }
 
@@ -46,8 +59,14 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnground = false;
             playAn.SetTrigger("Jump_trig");
+            ps_dirty.Stop();
+            as_player.PlayOneShot(ac_jump, 1.0f);
         }
-        if (!isOnground) playAn.SetBool("Lean_b", true);
+        if (!isOnground)
+        {
+            playAn.SetBool("Lean_b", true);
+            ps_dirty.Stop();
+        }
         else playAn.SetBool("Lean_b", false);
     }
 
